65502cb6ccbcf706ad2de7a362ae97402299eb5c
Neev lila <141387755+neevlila@users.noreply.github.com>
2023-10-23 10:01:57 +0530

    c1.cpp

---
+++ c1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class vehicle
+{
+	public:
+		vehicle(){
+     	    cout<<"220130318021"<<endl;
+			cout<<"Lila Neev"<<endl;
+			cout<<"This is vehicle";
+		}
+};
+class car : public vehicle{
+};
+int main()
+{
+	car obj;
+}
